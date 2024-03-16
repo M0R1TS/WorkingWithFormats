7d40bff9667978f60d6612d611f691ac28489e14
morits <mor1ts@mail.ru>
2024-03-16 18:19:50 +0300

    Debugging backgrounds, buttons and pictures

--- app/src/main/java/ru/devsokovix/myapplication/MainActivity.kt
+++ app/src/main/java/ru/devsokovix/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.devsokovix.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun onClickToast(view: View){
+        Toast.makeText(this, "Я буду Android разработчиком", Toast.LENGTH_LONG).show()
     }
 }
